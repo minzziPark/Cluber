db648ca97174a0338631b1debe77813619b50215
minzziPark <minzzi0068@gmil.com>
2023-07-26 15:24:24 +0900

    create react app

---
+++ cluber/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes></Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
