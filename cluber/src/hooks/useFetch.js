069719f3fb97d9a468d1f4bf068c335eb2483478
minzziPark <minzzi0068@gmail.com>
2023-08-12 18:52:36 +0900

    js updated

---
+++ cluber/src/hooks/useFetch.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, [url]);
+
+  return data;
+}
